c3f5b67bf7dc2d96d5dbd7ecab0f7b45afdfd4aa
phil <uniphil@gmail.com>
2015-09-15 23:36:09 -0400

    Add react-leaflet

--- app/scripts/components/root.jsx
+++ app/scripts/components/root.jsx
@@ -2,9 +2,17 @@
 
 import React from 'react';
 import {RouteHandler} from 'react-router';
+import {Map, TileLayer} from 'react-leaflet'
 
 export default class Root extends React.Component {
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <Map style={{height: 300}} center={[45.43, -75.71]} zoom={12} >
+          <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' />
+        </Map>
+      </div>
+    )
   }
 }
