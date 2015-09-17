076987563a631f2a53a63db2085f1c870bb7d843
phil <uniphil@gmail.com>
2015-09-16 20:48:38 -0400

    Show a map

--- app/scripts/components/dashboard/waterpoints.jsx
+++ app/scripts/components/dashboard/waterpoints.jsx
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import {Map, TileLayer} from 'react-leaflet';
 import T from '../utils/t.jsx';
 import ChartsContainer from './charts-container.jsx';
 
@@ -12,6 +13,12 @@ export default class WaterPoints extends Component {
       <div className="waterpoints">
         <h1><T k="dash.waterpoints" /></h1>
         <Link to="/waterpoints/some-waterpoint-id">Some waterpoint</Link>
+        <Map
+            style={{height: 240}}
+            center={[-6.3, 35]}
+            zoom={5}>
+          <TileLayer url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </Map>
         {this.props.children}
         <ChartsContainer>
           charts for waterpoints...
