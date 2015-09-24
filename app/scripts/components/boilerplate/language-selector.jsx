47771f02116f7fdc42d85f61881a9e8eac02965a
esoliani <esoliani@aiddata.org>
2015-09-24 16:33:10 -0300

    fix review comment

--- app/scripts/components/boilerplate/language-selector.jsx
+++ app/scripts/components/boilerplate/language-selector.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import classNames from 'classnames';
 import T from '../utils/t';
 
 require('stylesheets/boilerplate/language-selector');
 
 const LanguageSelector = React.createClass({
+  getInitialState() {
+    return {lang: 'en', langList: ['en', 'sw-tz']};
+  },
+
+  handleClick(lang) {
+    // TODO to be implemented
+  },
+
   render() {
     return (
       <ul className="language-selector">
-        <li><T k="lang.en" /></li>
-        <li><T k="lang.sw-tz" /></li>
+        {this.state.langList.map(function(item, index) {
+          const classes = classNames({
+            'language-selector': true,
+            'active': this.state.lang === item,
+          });
+          return <li className={classes}  key={index} onClick={this.handleClick.bind(this, item)}><T k={`lang.${item}`} /></li>;
+        }, this)}
       </ul>
     );
   },
