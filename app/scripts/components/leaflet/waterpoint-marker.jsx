f9dc6665fe3ac1ee2d9f9613d3d92c6b0d37746a
phil <uniphil@gmail.com>
2015-09-24 09:32:58 -0400

    Extend a leaflet component instead of wrapping

--- app/scripts/components/leaflet/waterpoint-marker.jsx
+++ app/scripts/components/leaflet/waterpoint-marker.jsx
@@ -1,28 +1,57 @@
 import React, { PropTypes } from 'react';
-import { Marker, Popup } from 'react-leaflet';
-import { Map } from 'leaflet';
+import { Popup } from 'react-leaflet';
+import PopupContainer from 'react-leaflet/PopupContainer';
+import { Map, Icon, marker } from 'leaflet';
 
-const WaterpointMarker = React.createClass({
-  propTypes: {
+export default class WaterpointMarker extends PopupContainer {
+  static propTypes = {
     LATITUDE: PropTypes.number.isRequired,
     LONGITUDE: PropTypes.number.isRequired,
     WATER_POINT_CODE: PropTypes.string.isRequired,
     WATER_POINT_NAME: PropTypes.string.isRequired,
+    icon: PropTypes.instanceOf(Icon),
     map: PropTypes.instanceOf(Map),  // injectd by <Map>
-  },
+    opacity: PropTypes.number,
+    zIndexOffset: PropTypes.number,
+  };
+
+  componentWillMount() {
+    super.componentWillMount();
+    const { map, LATITUDE, LONGITUDE, ...props } = this.props;
+    this.leafletElement = marker([LATITUDE, LONGITUDE], props);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { LATITUDE, LONGITUDE } = this.props;
+    if (LATITUDE !== prevProps.LATITUDE ||
+        LONGITUDE !== prevProps.LONGITUDE) {
+      this.leafletElement.setLatLng([LATITUDE, LONGITUDE]);
+    }
+    if (this.props.icon !== prevProps.icon) {
+      this.leafletElement.setIcon(this.props.icon);
+    }
+    if (this.props.zIndexOffset !== prevProps.zIndexOffset) {
+      this.leafletElement.setZIndexOffset(this.props.zIndexOffset);
+    }
+    if (this.props.opacity !== prevProps.opacity) {
+      this.leafletElement.setOpacity(this.props.opacity);
+    }
+  }
+
   render() {
-    const { map, LATITUDE, LONGITUDE } = this.props;
+    const reactLeafletProps = {
+      map: this.props.map,
+      popupContainer: this.leafletElement,
+    };
     return (
-      <Marker map={map} position={[LATITUDE, LONGITUDE]}>
-        <Popup map={map}>
+      <div style={{display: 'none'}}>
+        <Popup {...reactLeafletProps}>
           <div>
             <h2>hello my name is {this.props.WATER_POINT_NAME}</h2>
             <p>I am a waterpoint</p>
           </div>
         </Popup>
-      </Marker>
+      </div>
     );
-  },
-});
-
-export default WaterpointMarker;
+  }
+}
