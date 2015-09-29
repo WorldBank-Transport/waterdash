61d4861a293e4226d30184b4c535b0c4123b9f6d
phil <uniphil@gmail.com>
2015-09-29 12:04:50 -0400

    Move metricStatus out of charts body so it is visible when they are closed.

--- app/scripts/components/dashboard/waterpoints.jsx
+++ app/scripts/components/dashboard/waterpoints.jsx
@@ -9,7 +9,6 @@ import { TileLayer } from 'react-leaflet';
 import BoundsMap from '../leaflet/bounds-map';
 import WaterpointMarker from '../leaflet/waterpoint-marker';
 import ChartsContainer from './charts-container';
-import MetricStatus from './charts/metric-status';
 import SpinnerModal from '../misc/spinner-modal';
 
 require('stylesheets/dashboard/waterpoints');
@@ -46,9 +45,7 @@ const WaterPoints = React.createClass({
         <ChartsContainer
             onToggle={toggleCharts}
             state={this.state.layout.charts}>
-          <MetricStatus metric="54.65" title="chart.title.functional" total="123456"/>
-          <MetricStatus metric="54.65" title="chart.title.functional" total="123456"/>
-          <MetricStatus metric="54.65" title="chart.title.functional" total="123456"/>
+          charts go here...
         </ChartsContainer>
       </div>
     );
