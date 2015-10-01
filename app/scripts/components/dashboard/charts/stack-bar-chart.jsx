af01222a65df745d2db88a78406271c4084cac32
esoliani <esoliani@aiddata.org>
2015-10-01 11:24:16 -0300

    add chart jsx

---
+++ app/scripts/components/dashboard/charts/stack-bar-chart.jsx
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+import {BarChart} from 'react-d3';
+import * as func from '../../../utils/functional';
+
+const StackBarChart = React.createClass({
+  propTypes: {
+    data: PropTypes.object,
+  },
+  render() {
+    const dataRes = func.Result.aggregate(this.props.data, 'STATUS', 'REGION');
+    return dataRes.andThen(data => {
+      if (data.keys.aggProps.length > 0) {
+        const barData = Object.keys(data)
+          .filter(key => key !== 'keys')
+          .map(key => {
+            return {
+              name: key,
+              values: data.keys.sumProps.map(status => {
+                return {
+                  x: status,
+                  y: data[key][status] ? data[key][status] : 0,
+                };
+              }),
+            };
+          });
+        return (<BarChart
+            data={barData}
+            fill={'#3182bd'}
+            height={200}
+            legend={true}
+            stackOffset={'wigget'}
+            title="Bar Chart"
+            width={500} />
+        );
+      } else {
+        return <div>no chart</div>;
+      }
+    });
+  },
+});
+
+export default StackBarChart;
