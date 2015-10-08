d18437d8293f40fc88b31d5a9532dffde6ceabaa
Gali <gkalvatcheva@developmentgateway.org>
2015-10-08 01:41:13 -0400

    • added divider to the other secondary pages

--- app/scripts/components/static/speak-out.jsx
+++ app/scripts/components/static/speak-out.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Divider from '../boilerplate/divider';
 
 const SpeakOut = React.createClass({
   render() {
     return (
       <div className="main">
+        <Divider />
         <h1>Speak Out Page</h1>
       </div>
     );
