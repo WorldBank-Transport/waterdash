80cddc3ca608b8fbe7aa0691ee3683dd09e09b00
Gali <gkalvatcheva@developmentgateway.org>
2015-10-28 09:52:44 -0400

    • separated the external links into a component ( looking at ways to create this in a better way similar to the reusable button component) • replacement of logos

--- app/scripts/components/static/data.jsx
+++ app/scripts/components/static/data.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Divider from '../boilerplate/divider';
+import DataSources from '../boilerplate/external-links';
 import T from '../misc/t';
 
 require('stylesheets/boilerplate/static-content');
@@ -12,11 +13,7 @@ const Data = React.createClass({
         <div className="static-content">
         <h2><T k="static.data-title" /></h2>
         <p><T k="static.data-content" /></p>
-          <ul className="data-sources">
-            <li><a href="http://www.nbs.go.tz/"><img src="images/tz-odp.png"/></a></li>
-            <li><a href="http://www.maji.go.tz/"><img src="images/water-ministry.png"/></a></li>
-            <li><a href="http://www.nbs.go.tz/"><img src="images/nbs.png"/></a></li>
-          </ul>
+          <DataSources />
         </div>
       </div>
     );
