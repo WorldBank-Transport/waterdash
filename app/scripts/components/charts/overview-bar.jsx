17b6f1a6e001bdbbef243e456f5bc867b563f6fb
phil <uniphil@gmail.com>
2015-10-28 21:59:54 -0400

    Restore waterpoints status indicators

--- app/scripts/components/charts/overview-bar.jsx
+++ app/scripts/components/charts/overview-bar.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import T from '../misc/t';
+import React, { PropTypes } from 'react';
+import { _ } from 'results';  // catch-all for match
+import * as func from '../../utils/functional';
+import DataTypes from '../../constants/data-types';
+import MetricStatus from './metric-status';
 
-require('stylesheets/boilerplate/overview-bar');
+require('stylesheets/charts/overview-bar');
 
 const OverviewBar = React.createClass({
+  propTypes: {
+    data: PropTypes.array.isRequired,
+    dataType: PropTypes.instanceOf(DataTypes.OptionClass).isRequired,
+  },
+  renderWaterpoints() {
+    const data = func.Result.countBy(this.props.data, 'STATUS');
+    return (
+      <div className="waterpoints-metrics">
+        <MetricStatus metric="FUNCTIONAL" sumProps={data} title="chart.title.functional" />
+        <MetricStatus metric="FUNCTIONAL NEEDS REPAIR" sumProps={data} title="chart.title.repair" />
+        <MetricStatus metric="NON FUNCTIONAL" sumProps={data} title="chart.title.non-functional" />
+      </div>
+    );
+  },
+  renderOther() {
+    return 'not yet implemented';
+  },
   render() {
     return (
       <div className="overview-bar">
-        <T k="overview-bar" />
+        {DataTypes.match(this.props.dataType, {
+          Waterpoints: this.renderWaterpoints,
+          [_]: this.renderOther,
+        })}
       </div>
     );
   },
