f7c7e0d0b20d2da4cc66f4d507dbad4ae6b25509
Ernesto Soliani <ernestosoliani@gmail.com>
2015-11-17 11:33:32 -0300

    add dams overview bar

---
+++ app/scripts/components/charts/dams-overview-bar.jsx
@@ -0,0 +1,64 @@
+import React, { PropTypes } from 'react';
+import * as func from '../../utils/functional';
+import OpenClosed from '../../constants/open-closed';
+import T from '../misc/t';
+
+require('stylesheets/charts/dams-overview-bar');
+
+const DamsOverviewBar = React.createClass({
+  propTypes: {
+    data: PropTypes.array,  // injected
+    openClosed: PropTypes.instanceOf(OpenClosed.OptionClass),
+  },
+
+  renderMetrics(metrics) {
+    return Object.keys(metrics).map(metric => {
+      return (
+        <div className={`metric-status ${metrics[metric].className}`}>
+          <div className="icon">
+            {metrics[metric].iconSymbol}
+          </div>
+          <div className="content">
+            <div className="big-number">
+              <span className="number">{metrics[metric].data[metric]}</span>
+            </div>
+            <div className="context">
+              <T k={metrics[metric].title} />
+            </div>
+          </div>
+        </div>);
+    });
+  },
+
+  render() {
+    const metrics = {
+      RESERVOIR_: {
+        className: 'dams-reservoir',
+        data: func.Result.sumBy(this.props.data, 'RESERVOIR_'),
+        iconSymbol: 'R',
+        title: 'chart.title.reservoir',
+      },
+      DAM_HEIGHT: {
+        className: 'dams-height',
+        data: func.Result.sumBy(this.props.data, 'DAM_HEIGHT'),
+        iconSymbol: 'H',
+        title: 'chart.title.height',
+      },
+      ELEVATION_: {
+        className: 'dams-elevation',
+        data: func.Result.sumBy(this.props.data, 'ELEVATION_'),
+        iconSymbol: 'E',
+        title: 'chart.title.elevation',
+      },
+    };
+    return (
+      <div className="charts-container-summary">
+        <div>
+          {this.renderMetrics(metrics)}
+        </div>
+      </div>
+    );
+  },
+});
+
+export default DamsOverviewBar;
