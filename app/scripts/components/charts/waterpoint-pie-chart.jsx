737c23a7e626e33e8f84d5271fccfcd9e54f4273
Ernesto Soliani <ernestosoliani@gmail.com>
2015-11-24 17:54:37 -0300

    add more tyle and tooltip to pie chart

--- app/scripts/components/charts/waterpoint-pie-chart.jsx
+++ app/scripts/components/charts/waterpoint-pie-chart.jsx
@@ -3,6 +3,7 @@ import * as func from '../../utils/functional';
 import ShouldRenderMixin from '../../utils/should-render-mixin';
 import {PieChart} from 'react-d3-components';
 import T from '../misc/t';
+import { getNumberOr0 } from '../../utils/number';
 
 require('stylesheets/charts/waterpoint-pie-chart');
 
@@ -14,8 +15,7 @@ const WaterpointPieChart = React.createClass({
 
   mixins: [ShouldRenderMixin],
 
-  parseData() {
-    const data = func.Result.countBy(this.props.data, this.props.column);
+  parseData(data) {
     const values = Object.keys(data)
                     .filter(key => key !== 'total')
                     .map(key => {
@@ -30,14 +30,45 @@ const WaterpointPieChart = React.createClass({
     };
   },
 
+  tooltip(x, dataRes) {
+    // const subItems = Object.keys(dataRes)
+    //                         .filter(key => key !== x && key !== 'total')
+    //                         .map(key => {
+    //   const percentage = (getNumberOr0(dataRes[key]) / dataRes.total * 100).toFixed(2);
+    //   return (<li>
+    //             <spam className="metric-title">{{key}}:</spam>
+    //             <div className="waterpoint-tooltip-stat-wrapper">
+    //               <span className="number">{getNumberOr0(dataRes[key])}</span><span className="percent-value-wrapper">(<span className="number">{percentage}</span> %)</span>
+    //             </div>
+    //           </li>);
+    // });
+    const percentage = (getNumberOr0(dataRes[x]) / dataRes.total * 100).toFixed(2);
+    return (<div>
+                <ul className="items">
+                  <li>
+                    <spam className="metric-title selected">{x}:</spam>
+                    <div className="waterpoint-tooltip-stat-wrapper">
+                      <span className="number">{getNumberOr0(dataRes[x])}</span><span className="percent-value-wrapper">(<span className="number">{percentage}</span> %)</span>
+                    </div>
+                  </li>
+              </ul>
+            </div>);
+  },
+
   render() {
+    const data = func.Result.countBy(this.props.data, this.props.column);
     return (
       <div className="waterpoint-pie-chart">
         <h3 className="chart-title"><T k={`chart.pie.${this.props.column}`} /></h3>
         <PieChart
-            data={this.parseData()}
-            height={300}
-            margin={{top: 10, bottom: 50, left: 50, right: 10}}
+            data={this.parseData(data)}
+            height={400}
+            labelRadius={130}
+            margin={{top: 50, bottom: 50, left: 50, right: 50}}
+            tooltipContained="true"
+            tooltipHtml={(x) => this.tooltip(x, data)}
+            tooltipMode="mouse"
+            tooltipOffset={{top: -60, left: 0}}
             width={400}/>
       </div>
     );
