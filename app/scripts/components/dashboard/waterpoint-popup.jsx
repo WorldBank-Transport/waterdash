f42be785141827c20f84fa3a7a8ad3f8d0e1896a
Gali <gkalvatcheva@developmentgateway.org>
2015-11-30 15:43:51 -0500

    • new icon for population served • removed the dummy percentage stat ( this should be a number) • added stat icons next the the labels instead of values • used font awesome for stat icons

--- app/scripts/components/dashboard/waterpoint-popup.jsx
+++ app/scripts/components/dashboard/waterpoint-popup.jsx
@@ -41,9 +41,10 @@ const WaterpointPopup = React.createClass({
         <div className="row population-ratio">
             <h3>WATERPOINT TO POPULATION RATIO</h3>
             <div className="popup-stat-container">
-              <span className="big-number">73%</span>
-              <span className="population served"></span>
-              <span className="population not-served"></span>
+              <span className="big-number">45,000</span>
+              <span className="stat-symbol population-big">
+                <img src="images/population-icon.png"/>
+              </span>
             </div>
         </div>
 
@@ -54,44 +55,48 @@ const WaterpointPopup = React.createClass({
 
         <div className="row">
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.population-served" /></h3>
-            <span className="big-number">{this.props.waterpoint['POPULATION SERVED']}</span>
+            <h3>
+            <span className="stat-symbol population-small">
+              <img src="images/population-icon.png"/>
+            </span>
+            <T k="popup.waterpoint.population-served" /></h3>
+            <span className="popup-stat">{this.props.waterpoint['POPULATION SERVED']}</span>
           </div>
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.hardware-problem" /></h3>
-            <span className="big-number">{this.props.waterpoint.HARDWARE_PROBLEM}</span>
+            <h3><span className="fa fa-wrench"></span><T k="popup.waterpoint.hardware-problem" /></h3>
+            <span className="popup-stat">{this.props.waterpoint.HARDWARE_PROBLEM}</span>
           </div>
         </div>
 
         <div className="row bordered">
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.quantity" /></h3>
-            <span className="big-number">{this.props.waterpoint.WATER_QUANTITY}</span>
+            <h3><span className="fa fa-tint"></span><T k="popup.waterpoint.quantity" /></h3>
+            <span className="popup-stat">{this.props.waterpoint.WATER_QUANTITY}</span>
           </div>
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.quality" /></h3>
-            <span className="big-number">{this.props.waterpoint.WATER_QUALITY}</span>
+            <h3><span className="fa fa-tint"></span><T k="popup.waterpoint.quality" /></h3>
+            <span className="popup-stat">{this.props.waterpoint.WATER_QUALITY}</span>
           </div>
         </div>
 
         <div className="row">
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.position" /></h3>
+            <h3><span className="fa fa-dot-circle-o"></span><T k="popup.waterpoint.position" /></h3>
             <span className="location-text">{JSON.stringify(this.props.waterpoint.position)}</span>
           </div>
           <div className="popup-col">
-          <h3><T k="popup.waterpoint.region" /></h3>
+          <h3><span className="fa fa-map-marker"></span><T k="popup.waterpoint.region" /></h3>
           <span className="location-text">{this.props.waterpoint.REGION}</span>
           </div>
         </div>
 
         <div className="row">
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.district" /></h3>
+            <h3><span className="fa fa-map-marker"></span><T k="popup.waterpoint.district" /></h3>
             <span className="location-text">{this.props.waterpoint.DISTRICT}</span>
           </div>
           <div className="popup-col">
-            <h3><T k="popup.waterpoint.ward" /></h3>
+            <h3><span className="fa fa-map-marker"></span><T k="popup.waterpoint.ward" /></h3>
             <span>{this.props.waterpoint.WARD} / {this.props.waterpoint.VILLAGE} / {this.props.waterpoint.SUB_VILLAGE}</span>
           </div>
         </div>
