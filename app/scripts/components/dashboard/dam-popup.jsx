a36898da81d37e20c1b81ed6803777994875abad
Gali <gkalvatcheva@developmentgateway.org>
2015-12-09 18:09:15 +0100

    styles for dams popup

--- app/scripts/components/dashboard/dam-popup.jsx
+++ app/scripts/components/dashboard/dam-popup.jsx
@@ -12,42 +12,45 @@ const DamPopup = React.createClass({
   render() {
     return (
       <div className="dam-popup">
-        <div className="row header">
-          <h3 className="main-header"><T k="popup.dam.name" />: {this.props.dam.DAM_NAME}</h3>
-          <span className="location-text"><T k="popup.dam.basin" />: {this.props.dam.BASIN}</span>
+        <div className="popup-header">
+          <img className="dam-popup-icon" src="images/dams.png"/>
+          <h3><T k="popup.dam.name" />: {this.props.dam.DAM_NAME}</h3>
+          <span className="secondary-text"><T k="popup.dam.basin" />: {this.props.dam.BASIN}</span>
         </div>
-        <div className="row header">
-          <div className="row">
-            <h3 className="main-header"><T k="popup.dam.elevation" /></h3>
-            <span className="medium-number">{this.props.dam.ELEVATION_}</span>
-          </div>
-          <div className="row">
-            <h3 className="main-header"><T k="popup.dam.height" /></h3>
-            <span className="medium-number">{this.props.dam.DAM_HEIGHT}</span>
-          </div>
-          <div className="row">
-            <h3 className="main-header"><T k="popup.dam.reservoir" /></h3>
-            <span className="medium-number">{this.props.dam.RESERVOIR_}</span>
-          </div>
-        </div>
-        <div className="row header">
-          <div>
-            <h3 className="second-header"><T k="popup.dam.position" /></h3>
-            <span className="location-text">{JSON.stringify(this.props.dam.position)}</span>
-          </div>
-        </div>
+
         <div className="row">
-          <div>
-            <h3 className="second-header"><T k="popup.dam.region" /></h3>
-            <span className="location-text">{this.props.dam.REGION}</span>
+          <div className="popup-col">
+            <h3><img className="dam-popup-icon small" src="images/dams-blue.png"/><T k="popup.dam.elevation" /></h3>
+            <span className="popup-stat">{this.props.dam.ELEVATION_}</span>
+          </div>
+          <div className="popup-col">
+            <h3><img className="dam-popup-icon small" src="images/dams-blue.png"/><T k="popup.dam.height" /></h3>
+            <span className="popup-stat">{this.props.dam.DAM_HEIGHT}</span>
           </div>
         </div>
+
         <div className="row">
-          <div>
-            <h3 className="second-header"><T k="popup.dam.district" /></h3>
-            <span className="location-text">{this.props.dam.DISTRICT}</span>
+          <div className="popup-col">
+            <h3><span className="fa fa-tint"></span><T k="popup.dam.reservoir" /></h3>
+            <span className="popup-stat">{this.props.dam.RESERVOIR_}</span>
+          </div>
+          <div className="popup-col">
+            <h3><span className="fa fa-dot-circle-o"></span><T k="popup.dam.position" /></h3>
+            <span className="popup-stat">{JSON.stringify(this.props.dam.position)}</span>
           </div>
         </div>
+
+        <div className="row">
+          <div className="popup-col">
+            <h3><span className="fa fa-map-marker"></span><T k="popup.dam.region" /></h3>
+            <span className="popup-stat">{this.props.dam.REGION}</span>
+          </div>
+          <div className="popup-col">
+            <h3><span className="fa fa-map-marker"></span><T k="popup.dam.district" /></h3>
+            <span className="popup-stat">{this.props.dam.DISTRICT}</span>
+          </div>
+        </div>
+
       </div>
     );
   },
