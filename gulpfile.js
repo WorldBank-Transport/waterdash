462ec8c51afd915d223295b33c6f65040c596cae
Ernesto Soliani <ernestosoliani@gmail.com>
2015-12-16 10:17:30 -0300

    download the file from ckan and save it on the app

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sassLint = require('gulp-sass-lint');
 const inject = require('gulp-inject');
 const clean = require('gulp-clean');
 const ghPages = require('gulp-gh-pages');
+const exec = require('gulp-exec');
 
 gulp.task('lint-styles', function() {
   gulp.src([
@@ -25,6 +26,23 @@ gulp.task('copy:index', function() {
 gulp.task('clean:dist', function () {
     return gulp.src('dist', {read: false})
         .pipe(clean());
+});
+
+gulp.task('download', function() {
+  var filesId = {
+    'ckan-boreholes.json': 'c9843a61-eca6-47bb-971d-70bf9c0fe942',
+    'ckan-dams.json': '5da4eb70-47a0-4694-b735-397bb3732b99',
+    'ckan-population.json': 'ab84afa2-0afa-411e-9630-aeddc7bccb03',
+    'ckan-waterpoints.json': 'a94b3653-55f4-4455-9bed-42b92d5c4370',
+  };
+  var reportOptions = {
+    err: true, // default = true, false means don't write err 
+    stderr: true, // default = true, false means don't write stderr 
+    stdout: true // default = true, false means don't write stdout 
+  };
+  gulp.src('app/data/**/*', {read: false})
+    .pipe(exec('wget -O <%= file.path %>  "http://data.takwimu.org/api/action/datastore_search?resource_id=<%= options[file.path.substring(file.base.length, file.path.length)] %>&limit=100000"', filesId))
+    .pipe(exec.reporter(reportOptions));
 });
 
 gulp.task('copy:static', function() {
